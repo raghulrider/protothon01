5430bb39a76a50d9c2ae4b444c749143bf5716d1
Raghul Prasad Venkatachalam <raghulprasadv@gmail.com>
2019-03-16 13:20:40 +0530

    Create Pascals_Triangle.c
    Pascals triangle C code

---
+++ Pascals_Triangle.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,value,rows;
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+    for(i=0;i<rows;i++)
+    {
+        for(j=1;j<=rows-i;j++)
+        {
+            printf("  ");
+        }
+        for(k=0;k<=i;k++)
+        {
+            if(i==0 || k==0)
+            {
+                value = 1;
+            }
+            else
+            {
+                value = value*(i-k+1)/k;
+            }
+            //printf("  "); //Uncomment this print line if you want clear answer for large number of rows
+            printf("%4d", value);
+        }
+        printf("  \n");
+    }
+    return(0);
+}
